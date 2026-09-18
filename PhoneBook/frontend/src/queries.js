508abfb263d8a1905aca916da9f3adb0a0df8ab2
mostafazhara0x0 <mostafazahra.root.0x0@gmail.com>
2026-09-18 22:54:14 +0300

    Add createPerson mutation and PersonForm component

--- PhoneBook/frontend/src/queries.js
+++ PhoneBook/frontend/src/queries.js
@@ -23,3 +23,26 @@ export const FIND_PERSON = gql`
     }
   }
 `
+export const CREATE_PERSON = gql`
+  mutation createPerson(
+    $name: String!,
+    $street: String!,
+    $city: String!,
+    $phone: String
+    ) {
+    addPerson(
+      name: $name,
+      street: $street,
+      city: $city,
+      phone: $phone
+    ) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
